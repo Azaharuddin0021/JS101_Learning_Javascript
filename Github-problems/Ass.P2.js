c6580dd5f3f0c3ff3c229f02a1aa36166af8059c
AZAHARUDDIN <azharaiet92@gmail.com>
2023-02-09 13:14:11 +0530

    1st repo on github

---
+++ Github-problems/Ass.P2.js
@@ -0,0 +1,9 @@
+// Problem 2: If a person is allowed to drive in India print "Apply for a license" or "NA".
+
+let allowed_drive_age= 18;
+let age=17;
+if(age>=allowed_drive_age){
+    console.log("Apply for a license");
+} else {
+    console.log("Not Eligible ")
+}
---
+++ Github-problems/Ass.P4.js
@@ -0,0 +1,40 @@
+// Problem 4: Given stored username and password and input username and password, Print if the user can login or not. 
+let user_name= "azharKhan";
+let password=" Raza123456"
+let input_user_name= "kuch bhi";
+let input_pass= "wrong";
+if((user_name===input_user_name)&&(password===input_pass)){
+    console.log("user can log in");
+} else{
+    console.log("user can not log in")
+}
+
+// let user_name= "azharKhan";
+// let password="Raza123456";
+// let input_user_name= "jazharKhan";
+// let input_pass= "Raza123456";
+//   if(user_name===input_user_name){
+//     console.log("user name correct");
+//     if(password===input_pass){
+//         console.log("password is correct");
+//     } else {
+//         console.log("password is wrong")
+//     }
+//   } else if(password===input_pass){
+//         console.log("password is correct");
+//     } else if(password!==input_pass) {
+//         console.log("password is wrong")
+//     } else{
+//       console.log("user name wrong")
+      
+//     }
+
+
+// if((user_name===input_user_name)&&(password===input_pass)){
+//     console.log("user can log in");
+// } else{
+//      console.log("user can not log in")
+// }
+
+
+
